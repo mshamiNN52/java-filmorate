1b1fb745b63e9a5646d8e189fde07c5da207f80e
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-05-11 23:20:45 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@Validated
+@Slf4j
+public class UserController {
+
+    Map<Integer,User> users = new HashMap<>();
+
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        log.debug("Запрос списка пользователей");
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody @Validated User user)  {
+        if(user.getName().isEmpty()) {
+            log.debug("Добавление пользователя");
+            user.setName(user.getLogin());
+            users.put(user.getId(), user);
+        }else {
+            users.put(user.getId(), user);
+        }
+        return user;
+    }
+
+    @PutMapping("/user")
+    public User update(@RequestBody @Validated  User user) {
+        for (User users1 : users.values()) {
+            if (user.getId().equals(users1.getId())) {
+                log.debug("Обновление данных пользователя");
+                users.replace(user.getId(), user);
+            } else {
+                log.debug("пользователь  не найден");
+                System.out.println("No Find");
+            }
+        }
+        return user;
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/DurationTypeAdapter.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationTypeAdapter extends TypeAdapter<Duration> {
+
+    @Override
+    public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
+
+        jsonWriter.value(duration.toString());
+
+    }
+
+    @Override
+    public Duration read(JsonReader jsonReader) throws IOException {
+        return Duration.parse(jsonReader.nextString());
+    }
+}
+
