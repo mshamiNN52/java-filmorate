a25af49da6626827ff30d32a69b47cac9f591e0b
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-05-25 22:15:28 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,70 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+
+    private final FilmService filmService;
+    private final UserService userService;
+
+    @Autowired
+    public FilmController(FilmService filmService, UserService userService) {
+        this.filmService = filmService;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film createFilm(@Valid @RequestBody Film film) {
+        return filmService.createFilm(film);
+    }
+
+    @GetMapping
+    public List<Film> findAll() {
+        return filmService.getAllFilms();
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        return filmService.updateFilm(film);
+    }
+
+    @DeleteMapping
+    public void deleteFilm(@RequestBody Film film) {
+        filmService.deleteFilm(film);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void likeFilm(@PathVariable("id") int filmId, @PathVariable("userId") int userId) {
+        filmService.findFilmById(filmId);
+        userService.findUserById(userId);
+        filmService.likeFilm(filmId, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable("id") int filmId, @PathVariable("userId") int userId) {
+        filmService.findFilmById(filmId);
+        userService.findUserById(userId);
+        filmService.deleteLike(filmId, userId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> filmRate(@RequestParam(value = "count", defaultValue = "10") Integer count) {
+        return filmService.filmRate(count);
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilm(@PathVariable("id") int id) {
+        return filmService.findFilmById(id);
+    }
+}
